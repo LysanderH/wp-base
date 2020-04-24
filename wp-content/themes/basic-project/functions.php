1e429069e49664564fdcbb4e4f8fb699cc21e285
Lysander <lysander.hans@hotmail.com>
2020-04-24 12:13:10 +0200

    :construction: Added advanced menu

--- wp-content/themes/basic-project/functions.php
+++ wp-content/themes/basic-project/functions.php
@@ -3,28 +3,28 @@
 /**
  * Get theme assets
  */
-
 function bp_get_theme_asset($asset)
 {
     return get_stylesheet_directory_uri() . '/' . ltrim($asset, '/');
 }
 
+
+/**
+ * Get page title and heading
+ */
 function bp_get_title($separator = '-', $displayTitleLeft = true)
 {
-    $separator = '' . $separator = '';
-
-    $title = trim(wp_title('-', false, 'right'));
+    $title = trim(wp_title($separator, false, 'right'));
 
     if (!$title) {
         return get_bloginfo('name');
     }
 
     if ($displayTitleLeft) {
-        return trim(wp_title('-', false, 'right') . $separator . get_bloginfo('name'));
+        return trim(wp_title($separator, false, 'right') . get_bloginfo('name'));
     } else {
-        return get_bloginfo('name') . $separator . wp_title('-', false, 'left');
+        return get_bloginfo('name') . wp_title($separator, false, 'left');
     }
-
 }
 
 /**
@@ -39,4 +39,35 @@ register_nav_menu('main', 'Navigation principale du site');
 /***
  * Register socials menu
  */
-register_nav_menu('main', 'Navigation des liens pour les réseaux sociaux');
+register_nav_menu('socials', 'Navigation des liens pour les réseaux sociaux');
+
+/***
+ * Get menu items as array
+ */
+function bp_get_menu($location, $baseClass)
+{
+    global $object;
+    $items = [];
+    $locations = get_nav_menu_locations();
+    $id = $locations[$location];
+    $menu = wp_get_nav_menu_items($id);
+    /** Push each item of a link to an array */
+    foreach ($menu as $i => $object) {
+
+        $isTargettingHome = rtrim($object->url, '/') == rtrim(home_url(), '/');
+
+        $item = new stdClass();
+        $item->url = $object->url;
+        $item->label = $object->title;
+        $item->current = (is_home() && $isTargettingHome) || ($object->object_id == $object->ID);
+        $item->target = $object->target;
+        $item->classes = array_map(function ($item) use ($baseClass) {
+            return $baseClass . '--' . $item;
+        }, array_filter(array_merge([$item->current ? 'current' : null], $object->classes)));
+
+        array_unshift($item->classes, $baseClass);
+
+        $items[] = $item;
+    }
+    return $items;
+}
--- wp-content/themes/basic-project/header.php
+++ wp-content/themes/basic-project/header.php
@@ -9,12 +9,36 @@
     <?php wp_head(); ?>
     <title><?php wp_title('-', true, 'right'); ?><?php bloginfo('name'); ?></title>
     <title><?= bp_get_title('-', true); ?></title>
-    <link rel="stylesheet" href="<?= bp_get_theme_asset('assets/css/bundle.css');?>">
+    <link rel="stylesheet" href="<?= bp_get_theme_asset('assets/css/bundle.css'); ?>">
 </head>
 <body>
 <header class="header">
     <h1><?= bp_get_title('-', false); ?></h1>
-<?php wp_nav_menu(['theme_location'=>'main']); ?>
-<?php wp_nav_menu(['theme_location'=>'socials']); ?>
+    <nav class="nav">
+        <h2 class="nav__heading">Navigation principale</h2>
+        <div class="nav__container">
+            <?php foreach (bp_get_menu('main', 'nav__link') as $i => $link): ?>
+                <a href="<?= $link->url; ?>"
+                    <?php if ($link->target): ?> target="<?= $link->target; ?>" rel="noopener noreferrer"<?php endif; ?>
+                    <?php if ($link->current): ?> aria-current="page"<?php endif; ?>
+
+                   class="<?php if ($link->classes): ?>
+                   <?= implode('', $link->classes); ?>
+                   <?php endif; ?>"><?= $link->label; ?></a>
+            <?php endforeach; ?>
+        </div>
+    </nav>
+    <aside class="nav__socials">
+        <h3 class="nav__subheading"></h3>
+        <div class="nav__medias">
+            <?php foreach (bp_get_menu('socials', 'nav__social') as $i => $link): ?>
+                <a href="<?= $link->url; ?>"
+                    <?php if ($link->target): ?> target="<?= $link->target; ?>" rel="noopener noreferrer"<?php endif; ?>
+                   class="<?php if ($link->classes): ?>
+                   <?= implode('', $link->classes); ?>
+                   <?php endif; ?>"><?= $link->label; ?></a>
+            <?php endforeach; ?>
+        </div>
+    </aside>
 </header>
 <main>
