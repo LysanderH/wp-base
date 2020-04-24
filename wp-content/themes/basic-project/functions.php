d02127b2afb3753cf145bf1247b9dd8183836675
Lysander <lysander.hans@hotmail.com>
2020-04-24 09:30:12 +0200

    :construction: added custom title and heading function

--- wp-content/themes/basic-project/functions.php
+++ wp-content/themes/basic-project/functions.php
@@ -4,3 +4,21 @@ function bp_get_theme_asset($asset)
 {
     return get_stylesheet_directory_uri() . '/' . ltrim($asset, '/');
 }
+
+function bp_get_title($separator = '-', $displayTitleLeft = true)
+{
+    $separator = '' . $separator = '';
+
+    $title = trim(wp_title('-', false, 'right'));
+
+    if (!$title){
+        return get_bloginfo('name');
+    }
+
+    if ($displayTitleLeft) {
+        return trim(wp_title('-', false, 'right') . $separator . get_bloginfo('name'));
+    } else {
+        return get_bloginfo('name') . $separator . wp_title('-', false, 'left');
+    }
+
+}
--- wp-content/themes/basic-project/header.php
+++ wp-content/themes/basic-project/header.php
@@ -7,12 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!--  wp head not really beautiful  -->
     <?php wp_head(); ?>
-    <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
+    <title><?php wp_title('-', true, 'right'); ?><?php bloginfo('name'); ?></title>
+    <title><?= bp_get_title('-', true); ?></title>
     <link rel="stylesheet" href="<?= bp_get_theme_asset('assets/css/bundle.css');?>">
 </head>
 <body>
 <header class="header">
-    <h1><?php the_title(); ?></h1>
+    <h1><?= bp_get_title('-', false); ?></h1>
 
 </header>
 <main>
