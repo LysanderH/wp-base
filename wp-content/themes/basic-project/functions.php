b1abdf8b922cac13f16896b39aa30e48c12a7399
Lysander <lysander.hans@hotmail.com>
2020-05-08 09:28:31 +0200

    :construction: added custom post type and injection in view

--- wp-content/themes/basic-project/functions.php
+++ wp-content/themes/basic-project/functions.php
@@ -71,3 +71,21 @@ function bp_get_menu($location, $baseClass)
     }
     return $items;
 }
+
+/**
+ * Custom post type
+ */
+
+register_post_type('trip', [
+    'label' => 'voyage',
+    'labels' => [
+        'name' => 'Voyages',
+        'singular_name' => 'Voyage',
+        'add_new'=>'Ajouter nouveau',
+        'add_new_item'=>'Ajouter un nouveau voyage'
+    ],
+    'description' => 'Les récits de voyage que j’ai vécu',
+    'public' => true,
+    'menu_position'=>5,
+    'menu_icon'=>'dashicons-palmtree',
+]);
